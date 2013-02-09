9964582c34e1e262ccb2b98e4a8c906502434d4f
electronic max <electronic@gmail.com>
2013-02-09 23:18:15 +0000

    examples now histogramming

--- lec1-position/index.js
+++ lec1-position/index.js
@@ -1,11 +1,11 @@
 /*global $,_,document,window,console,escape,Backbone,exports */
 /*jslint vars:true, todo:true, sloppy:true */
 
-var N = 100;
+var N = 70;
 var Y0 = 0, Y1 = 20;
 var log = function() { console.log.apply(console,arguments); };
 var margin = {top: 10, right: 50, bottom: 20, left: 50},
-    width = 500 - margin.left - margin.right,
+    width = 900 - margin.left - margin.right,
     height = 100 - margin.top - margin.bottom;
 var gen_random = function() {
     var data = [];
@@ -17,17 +17,17 @@ var gen_random = function() {
 var series = gen_random(N);
 var make_vl = function(pts, arr) {
     arr = arr || [];
-    pts.map(function(x,i) {  arr[i] = [[x, Y0], [x, Y1]];	});
+    pts.map(function(x,i) {  arr[i] = [{x:x, y:Y0}, {x:x, y:Y1}];});
     return arr;
 };
 var origin_vl = make_vl([0]), series_vl = make_vl(series);
 var linscale = d3.scale.linear().domain([1,100]).range([0,width]); 
 var logscale = d3.scale.log().domain([1,100]).range([0,width]);
-
+var histscale = d3.scale.linear().domain([0,20]).range([0,50]);
 var linefn = function(mode) {
 	return line = d3.svg.line()
-		.x(function(d) { return mode(d[0]) + margin.left; })
-		.y(function(d) { return d[1] + margin.top; })
+		.x(function(d) { return mode(d).x + margin.left; })
+		.y(function(d) { return height - mode(d).y + margin.top; })
 		.interpolate("basis");
 	
 };
@@ -37,30 +37,41 @@ function ex1(sel) {
 		.enter().append('svg:path')
 		.attr('class', 'origin')
 		.attr('stroke', 'green')
-		.attr('d', linefn(linscale));
-	
+		.attr('d', linefn(function(xy) { return {x: linscale(xy.x), y: xy.y} }));
     d3.select(sel).selectAll('path.data').data(series_vl)
 		.enter().append('svg:path')
 		.attr('class', 'data')
-		.attr('stroke', 'blue')	
-		.attr('d', linefn(linscale));    
+		.attr('stroke', 'blue')
+		.attr('d', linefn(function(xy) { return {x: linscale(xy.x), y: xy.y} }));
 }
 
 var ex1_log = function() {
 	d3.select('svg.ex1').selectAll('path.data')
 		.transition().duration(1000)
-		.attr('d', linefn(logscale));
+		.attr('d', linefn(function(xy) { return {x: logscale(xy.x), y: xy.y} }));
 };
 var ex1_lin = function() {
 	d3.select('svg.ex1').selectAll('path.data')
 		.transition().duration(1000)
-		.attr('d', linefn(linscale));	
+		.attr('d', linefn(function(xy) { return {x: linscale(xy.x), y: xy.y} }));	
+};
+var BINSIZE = 5;
+var count_series_vl = function() {
+	var counts = {};
+	console.log('series vl ', series_vl);
+	series_vl.map(function(xy) {
+		var idx = Math.round(xy[0].x/BINSIZE)*BINSIZE;
+		counts[idx] = (counts[idx] || 0) + 1;
+	})
+	return counts;
 };
 var ex1_round = function() {
+	var counts = count_series_vl();
 	d3.select('svg.ex1').selectAll('path.data')
 		.transition().duration(1000)
-		.attr('d', linefn(function(t) {
-			return linscale(Math.round(t/10)*10);
+		.attr('d', linefn(function(t,i) {
+			var rx = Math.round(t.x/BINSIZE)*BINSIZE;
+			return { x: linscale(rx), y:t.y == 0 ? 0: histscale(counts[rx] || 0) + Y1};
 		}));
 };
 
@@ -77,8 +88,6 @@ var svg = d3.select("body").selectAll("svg")
     .each(function(render_fn) {
 		render_fn(this);
 	});
-
-
 
 // let's make some buttons! 
 d3.select('body').selectAll('div.btn')
