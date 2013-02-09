27a58d415c023fec898bbc4fa72619b3938a785a
electronic <electronic@sociam-skype.local>
2013-02-09 17:09:25 +0000

    ex1

--- lec1-position/index.js
+++ lec1-position/index.js
@@ -1,20 +1,55 @@
 
 var N = 100;
-var data = [];
-for (var i = 0; i < 100; i++) { data.push( 100*Math.random() ); }
-
-var examples = ['one', 'two'];
-
+var Y0 = 0, Y1 = 10;
 var margin = {top: 10, right: 50, bottom: 20, left: 50},
     width = 500 - margin.left - margin.right,
     height = 100 - margin.top - margin.bottom;
+var gen_random = function() {
+    var data = [];
+    for (var i = 0; i < N; i++) {
+	data.push( 100*Math.random() + 1 );
+    }
+    return data;
+};
+var series = gen_random(N);
 
-var chart = function(bloop, x) {
-    console.log('bloop ', bloop, x);
+// 13 -> [[x(13), 10], [x(13), 15]]
+var make_vl = function(pts) {
+    return  pts.map(function(x) { return [[x, Y0], [x, Y1]]; });
 };
 
-console.log('examples', examples);
+var origin_vl = make_vl([0]), series_vl = make_vl(series);
 
+function ex1(sel, mode) {
+    mode = mode || d3.scale.linear().domain([0,100]).range([0,width]);
+    
+    var line = d3.svg.line()
+	.x(function(d) { return mode(d[0]) + margin.left; })
+	.y(function(d) { return d[1] + margin.top; })
+	.interpolate("basis");
+    
+    d3.select(sel).selectAll('.origin')
+	.data(origin_vl)
+	.enter()
+	.append('svg:path')
+	.attr('stroke', 'green')
+	.attr('d', line);    
+    d3.select(sel).selectAll('.data')
+	.data(series_vl)
+	.enter()
+	.append('svg:path')
+	.attr('stroke', 'blue')
+	.transition()
+	.duration(1000)
+	.attr('d', line);    
+    console.log('selection is ', d3.select(sel));
+}
+
+function ex2(sel) {
+    console.log('ex2', sel);    
+}
+
+var examples = [ex1, ex2];
 var svg = d3.select("body").selectAll("svg")
     .data(examples)
     .enter()
@@ -23,11 +58,16 @@ var svg = d3.select("body").selectAll("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.bottom + margin.top)
     .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-    .call(chart);
+    .each(function(render_fn) { console.log('renderfn is ', typeof(render_fn), render_fn); render_fn(this);  });
 
-
-
+var ex1_log = function() {
+    series_vl = make_vl(series);    
+    d3.select("body").selectAll("svg")
+	.data(examples)
+	.each(function(render_fn) {
+		render_fn(this, d3.scale.log().domain([0,100]).range([0,width]));
+	    });    
+};
 
 
 
