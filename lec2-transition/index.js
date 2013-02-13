bed0e0459e055664b0dade348e4862a955b25c47
electronic max <electronic@gmail.com>
2013-02-13 18:07:03 +0000

    vis intro

--- lec2-transition/index.js
+++ lec2-transition/index.js
@@ -1,130 +1,74 @@
 /*global $,_,document,window,console,escape,Backbone,exports */
 /*jslint vars:true, todo:true, sloppy:true */
 
-var data = [
-	    { x: 0, y: 1, category: 'male' },
-	    { x: 1, y: 4, category: 'female' },
-	    { x: 2, y: 5, category: 'male' },
-	    { x: 3, y: 4, category: 'male' },
-	    { x: 4, y: 3, category: 'female' }
-	    { x: 5, y: 5, category: 'male' }
-	    { x: 6, y: 1, category: 'female' }
-	    { x: 7, y: 6, category: 'female' }
-	    ];
-
-var xscale = d3.scale.linear().domain([0,7]).range([0,width]); 
-var yscale = d3.scale.log().domain([1,100]).range([0,width]);
-
-var makelinefn = function(data) {
-    return = function(mode) {
-	var xmin = d3.min(data.
-	return line = d3.svg.line()
-	.x(function(d) { return xscale(d.x); })
-	.y(function(d) { return yscale(d.y); })
-	.interpolate("basis");
-    };
-};
-    
-var N = 70;
-var Y0 = 0, Y1 = 100;
-var log = function() { console.log.apply(console,arguments); };
-var margin = {top: 10, right: 50, bottom: 20, left: 50},
-    width = 900 - margin.left - margin.right,
-    height = 600 - margin.top - margin.bottom;
-
-var gen_random = function() {
-    var data = [];
-    for (var i = 0; i < N; i++) {
-		data.push( 100*Math.random() + 1 );
-    }
-    return data;
-};
-var series = gen_random(N);
-var make_vl = function(pts, arr) {
-    arr = arr || [];
-    pts.map(function(x,i) {  arr[i] = [{x:x, y:Y0}, {x:x, y:Y1}];});
-    return arr;
-};
-var origin_vl = make_vl([0]), series_vl = make_vl(series);
-var histscale = d3.scale.linear().domain([0,10]).range([0,height]);
-
-function ex1(sel) {
-    d3.select(sel).selectAll('path.origin').data(origin_vl)
-		.enter().append('svg:path')
-		.attr('class', 'origin')
-		.attr('stroke', 'green')
-		.attr('d', linefn(function(xy) { return {x: linscale(xy.x), y: xy.y} }));
-    d3.select(sel).selectAll('path.data').data(series_vl)
-		.enter().append('svg:path')
-		.attr('class', 'data')
-		.attr('stroke', 'blue')
-		.attr('d', linefn(function(xy) { return {x: linscale(xy.x), y: xy.y} }));
-}
-
-var ex1_log = function() {
-	d3.select('svg.ex1').selectAll('path.data')
-		.transition().duration(1000)
-		.attr('d', linefn(function(xy) { return {x: logscale(xy.x), y: xy.y} }));
-};
-var ex1_lin = function() {
-	d3.select('svg.ex1').selectAll('path.data')
-		.transition().duration(1000)
-		.attr('d', linefn(function(xy) { return {x: linscale(xy.x), y: xy.y} }));	
-};
-var BINSIZE = 5;
-var count_series_vl = function() {
-	var counts = {};
-	console.log('series vl ', series_vl);
-	series_vl.map(function(xy) {
-		var idx = Math.round(xy[0].x/BINSIZE)*BINSIZE;
-		counts[idx] = (counts[idx] || 0) + 1;
-	})
-	return counts;
-};
-var ex1_round = function() {
-	var counts = count_series_vl();
-	d3.select('svg.ex1').selectAll('path.data')
-		.transition().duration(1000)
-		.attr('d', linefn(function(t,i) {
-			var rx = Math.round(t.x/BINSIZE)*BINSIZE;
-			return { x: linscale(rx), y:t.y == 0 ? 0: histscale(counts[rx] || 0) + Y1};
-		}));
+var mklinefn = function(canvas, data) {
+    return function(mode) {
+		var xmin = d3.min(data.map(function(d) { return d.x; })),
+		    xmax = d3.max(data.map(function(d) { return d.x; })),
+		    ymin = d3.min(data.map(function(d) { return d.y; })),
+		    ymax = d3.max(data.map(function(d) { return d.y; })),
+		    xscale = d3.scale.linear().domain([xmin,xmax]).range([10, $(canvas).width() - 10]),
+		    yscale = d3.scale.linear().domain([ymin,ymax]).range([10, $(canvas).height() - 10]);
+		return d3.svg.line()
+			.x(function(d) { return xscale(d.x); })
+			.y(function(d) { return yscale(d.y); })		
+			.interpolate('basis');
+	};
 };
 
-// initialise the examples
-var examples = [ex1];
-var svg = d3.select("body").selectAll("svg")
-    .data(examples)
-    .enter()
-    .append("svg")
-    .attr("class", "box ex1")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.bottom + margin.top)
-    .append("g")
-    .each(function(render_fn) {
-		render_fn(this);
+var make_el = function(make_el, cls, parent) {
+	var s = "<"+make_el+">" + "</"+make_el+">";
+	return $(s).appendTo(parent || $('body')).addClass(cls||'');
+};
+
+var drawPlot = function(canvas, cls, data_array) {
+	return d3.select(canvas)
+		.selectAll('path.'+cls)
+		.data(data_array)
+		.enter()
+		.append('path')
+		.attr('class', cls)
+		.attr('d', mklinefn(canvas, data_array))
+		.attr('fill', 'blue')
+		.attr('stroke','black');
+};
+
+var transition_data = function(canvas, cls, new_data) {
+	return d3.select(canvas)
+		.selectAll('path.'+cls)
+		.data(new_data)
+		.transition()
+		.duration(1000)
+		.attr('d', mklinefn(canvas, new_data));
+};
+
+$(document).ready(function() {
+	var data = [
+		{ x: 0, y: 1, category: 'male' },
+		{ x: 1, y: 4, category: 'female' },
+		{ x: 2, y: 5, category: 'male' },
+		{ x: 3, y: 4, category: 'male' },
+		{ x: 4, y: 3, category: 'female' },
+		{ x: 5, y: 5, category: 'male' },
+		{ x: 6, y: 1, category: 'female' },
+		{ x: 7, y: 6, category: 'female' }
+	];	
+	var canvas = make_el('svg', 'box')[0];
+	window.canvas = canvas;
+	console.log('daaaaaaaaaata', canvas);	
+	// draw ourselves the path
+	var path = drawPlot(canvas, 'plot', data);
+	console.log('dlkfjdaaaaaaaaaata', canvas);	
+	var categories = _.uniq(data.map(function(x) { return x.category; }));
+	var button_group = make_el('div','btn-group').attr('data-toggle','buttons-radio');
+	console.log('categories ', categories);
+	categories.concat(['all']).map(function(c) {
+		make_el('div', 'btn', button_group)
+			.html(c)
+			.on('click', function() {
+				return transition_data(canvas, 'plot', data.filter(function(dx) {
+					return c === 'all' || dx.category === c;
+				}));
+			});
 	});
-
-// let's make some buttons! 
-var modegroup = d3.select('body').selectAll('div.modegroup')
-	.data(examples)
-	.enter().append('div')
-	.attr('class', 'modegroup btn-group')
-	.attr('data-toggle', 'buttons-radio');
-
-modegroup.selectAll('button.btn')
-	.data(['linear', 'log', 'round'])
-    .enter().append('div')
-	.attr('class', function(d) {
-		var isactive = d == 'linear' ? 'active ' : '';
-		return isactive + 'btn btn-primary ex1_' + d;
-	}).on('click', function(d) {
-	    if (d === 'log') { log('ex1 -> log'); return ex1_log(); }
-	    if (d === 'linear') { log('ex1 -> lin'); return ex1_lin(); }
-	    if (d === 'round') { log('ex1 -> rnd'); return ex1_round(); }	    
-	})
-    .text(function(d) { return d; });
-
-
-
-
+});
